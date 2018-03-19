c063d4587f4c62eee28cd166ce2a46f6bf62f147
atlantis <chinadch@gmail.com>
2018-03-19 21:44:12 +0800

    upgrade spring boot & angular

--- src/main/java/boot/service/impl/StudentServiceImpl.java
+++ src/main/java/boot/service/impl/StudentServiceImpl.java
@@ -6,29 +6,29 @@ import boot.respository.StudentRepository;
 import boot.service.StudentService;
 import boot.viewmodel.StudentSearch;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.UUID;
-
 @Service
 public class StudentServiceImpl implements StudentService {
-    @Autowired
-    private StudentRepository studentRepository;
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     private static Student convert(StudentEntity studentEntity) {
         ModelMapper modelMap = new ModelMapper();
-        Student student = modelMap.map(studentEntity, Student.class);
-        return student;
+        return modelMap.map(studentEntity, Student.class);
     }
 
     @Override
     public Page<Student> getAllStudents(StudentSearch studentSearch) {
-        Pageable pageable = new PageRequest(studentSearch.getPageIndex(), studentSearch.getPageSize());
+        Pageable pageable = PageRequest.of(studentSearch.getPageIndex(), studentSearch.getPageSize());
 
         if (!StringUtils.hasText(studentSearch.getName()))
             return studentRepository.findAll(pageable)
@@ -47,12 +47,11 @@ public class StudentServiceImpl implements StudentService {
             studentEntity.setGender(student.getGender());
             studentRepository.save(studentEntity);
         } else {
-            StudentEntity studentEntity = studentRepository.findOne(student.getId());
+            StudentEntity studentEntity = studentRepository.findById(student.getId()).orElse(null);
             studentEntity.setName(student.getName());
             studentEntity.setAge(student.getAge());
             studentEntity.setGender(student.getGender());
             studentRepository.save(studentEntity);
         }
-
     }
 }
